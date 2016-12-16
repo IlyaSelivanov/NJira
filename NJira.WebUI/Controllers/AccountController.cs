7b524d54871eaef6dfba841cbc605e9280489100
Ilya Selivanov <ilyaqs@quartsoft.com>
2016-12-16 16:07:38 +0200

    LogOff action; LoginPartial view; database configured to use local sql express server

--- NJira.WebUI/Controllers/AccountController.cs
+++ NJira.WebUI/Controllers/AccountController.cs
@@ -94,5 +94,14 @@ namespace NJira.WebUI.Controllers
             ViewBag.returnUrl = returnUrl;
             return View(model);
         }
+
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
