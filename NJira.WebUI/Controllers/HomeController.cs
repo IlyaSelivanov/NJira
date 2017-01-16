7c039868e076d7e59ee46d34584410bfe8e7f9ad
Ilya Selivanov <ilyaqs@quartsoft.com>
2017-01-16 16:15:40 +0200

    'Per Users' charts were added to the homepage

--- NJira.WebUI/Controllers/HomeController.cs
+++ NJira.WebUI/Controllers/HomeController.cs
@@ -27,30 +27,70 @@ namespace NJira.WebUI.Controllers
         public ActionResult Index()
         {
             var issues = from i in repository.Issues
-                         where i.FixVersions == "1.32.5"
+                         where i.FixVersions == "1.32.6"
                          orderby i.Status
                          select i;
 
-            string status = "";
+            string text = "";
             double total = (double)issues.Count();
             double count = 0;
             List<Tuple<string, double>> result = new List<Tuple<string, double>>();
 
             foreach (var issue in issues)
             {
-                if (!issue.Status.Name.Equals(status))
+                if (!issue.Status.Name.Equals(text))
                 {
-                    result.Add(new Tuple<string, double>( status, count));
-                    status = issue.Status.Name;
+                    result.Add(new Tuple<string, double>( text, count));
+                    text = issue.Status.Name;
                     count = 1;
                 }
                 else
                     count++;
             }
-            result.Add(new Tuple<string, double>(status, count));
+            result.Add(new Tuple<string, double>(text, count));
             result.RemoveAt(0);
 
             ViewBag.DataPoints = JsonConvert.SerializeObject(GetPieData(result, total), _jsonSetting);
+
+            text = "";
+            count = 0;
+            result.Clear();
+            foreach (var issue in issues.Where(i => i.Status == "Open").OrderBy(i => i.Assignee))
+            {
+                if (!issue.Assignee.Equals(text))
+                {
+                    result.Add(new Tuple<string, double>(text, count));
+                    text = issue.Assignee;
+                    count = 1;
+                }
+                else
+                    count++;
+            }
+            result.Add(new Tuple<string, double>(text, count));
+            result.RemoveAt(0);
+
+            ViewBag.DataPoints1 = JsonConvert.SerializeObject(GetColumnData(result, total), _jsonSetting);
+
+            text = "";
+            count = 0;
+            result.Clear();
+            foreach (var issue in issues.Where(i => i.Status == "Waiting for Review").OrderBy(i => i.Assignee))
+            {
+                if (!issue.Assignee.Equals(text))
+                {
+                    result.Add(new Tuple<string, double>(text, count));
+                    text = issue.Assignee;
+                    count = 1;
+                }
+                else
+                    count++;
+            }
+            result.Add(new Tuple<string, double>(text, count));
+            result.RemoveAt(0);
+
+            ViewBag.DataPoints2 = JsonConvert.SerializeObject(GetColumnData(result, total), _jsonSetting);
+
+            ViewBag.Total = total;
 
             return View();
         }
@@ -64,6 +104,20 @@ namespace NJira.WebUI.Controllers
                 _dataPoints.Add(new DataPoint(Math.Round((res.Item2 / count) * 100, 2),
                     res.Item1, 
                     string.Format("({0})", res.Item2.ToString())));
+            }
+
+            return _dataPoints;
+        }
+
+        private static List<DataPoint> GetColumnData(List<Tuple<string, double>> result, double count)
+        {
+            List<DataPoint> _dataPoints = new List<DataPoint>();
+
+            foreach (var res in result)
+            {
+                _dataPoints.Add(new DataPoint(res.Item2,
+                    res.Item1,
+                    res.Item1));
             }
 
             return _dataPoints;
